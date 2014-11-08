ec7fbe113d3e5e5d611d3003f8a201bd4df07d3b
Charanjit Singh <ckhabra007@gmail.com>
2014-11-08 18:31:57 +0530

    Added more sample routes

--- both/router.js
+++ both/router.js
@@ -3,7 +3,14 @@ Router.configure({
 });
 
 Router.route('/', function() {
-  this.render('home', {});
+  this.redirect('/nominees/all');
+});
+
+Router.route('/nominees/:category', function() {
+  var category = this.params.category || 'all';
+  this.render('home', {data: {category: category}});
+}, {
+  name: 'nominees'
 });
 
 Router.route('/nominee/:nomineeSlug', function() {
@@ -11,4 +18,18 @@ Router.route('/nominee/:nomineeSlug', function() {
   this.render('NomineeProfile', {data: {slug: slug}});
 }, {
   name: 'nomineeProfile'
+});
+
+
+Router.route('/login', function() {
+  if(Meteor.user() && Roles.userIsInRole(Meteor.userId(), ['admin']))
+    this.redirect('/admin');
+  else
+    if (Meteor.user()) this.redirect('/');
+  else
+    this.render('login');
+});
+
+Router.route('/admin', function() {
+  this.render("AdminDashboard");
 });
