ba4dd49c7bb6214ba5268896cd1969dcaf15727e
Charanjit Singh <ckhabra007@gmail.com>
2014-11-09 02:35:29 +0530

    Fix minor type

--- client/templates/js/home.js
+++ client/templates/js/home.js
@@ -6,7 +6,7 @@ Template.home.events({
   'click .vote-nominee': function(e) {
     e.preventDefault();
     if (!Meteor.user())
-      Router.go('login',{}, {query: "next='home'"});
+      Router.go('login',{}, {query: "next=home"});
     else
       Meteor.user().vote(this._id, 'web');
   }
