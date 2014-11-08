4cecc3be014fbe40f955834247665554b5c3aaea
Charanjit Singh <ckhabra007@gmail.com>
2014-11-09 03:47:25 +0530

    Done with Signup

--- client/templates/js/login.js
+++ client/templates/js/login.js
@@ -1,17 +1,50 @@
+Template.login.isSignupForm = function() {
+  return Session.get('loginForm') === 'signup';
+};
+
+Template.login.helpers({
+  actionBtnTxt: function() {
+    return Template.login.isSignupForm() ? 'Sign Up' : 'Sign In';
+  },
+  toggleFormTxt: function() {
+    return Template.login.isSignupForm() ? 'or Sign In' : 'or Sign Up';
+  },
+  signupForm: function() {
+    return Template.login.isSignupForm();
+  }
+});
+
 Template.login.events({
   'submit form': function (event, template) {
     if(this.query && this.query.next)
       var next = this.query.next;
 
     event.preventDefault();
-    var user = event.currentTarget[0].value;
+    var email = event.currentTarget[0].value;
     var password = event.currentTarget[1].value;
-    Meteor.loginWithPassword(user, password, function(err) {
-      if (err) {
-        alert("Couldn't log in: " + err.message);
+    var confirmPassword = event.currentTarget[2] && event.currentTarget[2].value;
+
+    if(Template.login.isSignupForm()) {
+      if(password !== confirmPassword) {
+        alert("Passwords didn't match");
         return;
-      } else
-        Router.go(next);
-    });
+      }
+
+      Accounts.createUser({
+        email: email,
+        password: password
+      });
+    } else {
+      Meteor.loginWithPassword(email, password, function(err) {
+        if (err) {
+          alert("Couldn't log in: " + err.message);
+          return;
+        } else
+          Router.go(next);
+      });
+    }
+  },
+  'click #toggleForm': function() {
+    Session.set('loginForm', Template.login.isSignupForm() ? 'signin' : 'signup');
   }
 });
