ca231d207bd1bebd522e870f6c2d9edf66fcfe20
Charanjit Singh <ckhabra007@gmail.com>
2014-11-08 21:52:14 +0530

    Added more dummy data

--- server/startup.js
+++ server/startup.js
@@ -5,41 +5,24 @@ Meteor.startup(function() {
   }
 
   if(Nominees.find().count() === 0) {
-    var n1 = new Nominee();
-    n1.first_name = "Nominee",
-    n1.last_name = "1",
-    n1.nominee_slug = 'nominee_1',
-    n1.type = 'volunteer',
-    n1.competition = 2014,
-    n1.vote_count = 0,
-    n1.email = 'vol1@vol.com',
-    n1.image = 'http://lorempixel.com/600/400/'
-    n1.description = (function() {
-      var desc = [] ;
-      _(100).times(function() {
-        desc.push('bla ');
+    _(40).times(function() {
+      Nominees.insert({
+        first_name: Random.id(),
+        last_name: 'Nom',
+        type: Random.choice(['ngo', 'volunteer', 'corporate']),
+        competition: 2014,
+        vote_count: Math.floor(Random.fraction()*100),
+        email: 'bla'+Math.floor(Random.fraction()*10)+'@bla.com',
+        image: 'http://lorempixel.com/' + Random.choice(['sports', 'food', 'city', 'people', 'cats']) + '/600/400/' ,
+        description: (function() {
+          var desc = [] ;
+          _(100).times(function() {
+            desc.push('bla ');
+          });
+          return desc.join();
+        })()
       });
-      return desc.join();
-    })();
-    n1.save();
-
-    var n2 = new Nominee();
-    n2.first_name = "Nominee",
-    n2.last_name = "2",
-    n2.nominee_slug = 'nominee_2',
-    n2.type = 'ngo',
-    n2.competition = 2014,
-    n2.vote_count = 0,
-    n2.email = 'ngo@ngo.com',
-    n2.image = 'http://lorempixel.com/600/400/'
-    n2.description = (function() {
-      var desc = [] ;
-      _(100).times(function() {
-        desc.push('bla ');
-      });
-      return desc.join();
-    })();
-    n2.save();
+    });
   }
 
   if(Meteor.roles.find().count() === 0) {
