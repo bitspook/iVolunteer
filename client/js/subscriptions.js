fbc72c88604b67bdba793540b6797d5e92234a0d
Charanjit Singh <ckhabra007@gmail.com>
2014-11-09 00:23:52 +0530

    Remove logs

--- client/js/subscriptions.js
+++
@@ -1,1 +0,0 @@
-Meteor.subscribe("nominee", Session.get("selected_nominee"));
--- server/publishers.js
+++ server/publishers.js
@@ -5,8 +5,6 @@ Meteor.publish('nominees', function(limit) {
 });
 
 Meteor.publish("nominee", function(nominee_id) {
-  console.log("SUBSCRIBING TO", nominee_id);
-
   if(! nominee_id) return false;
 
   return Nominees.find({_id: nominee_id});
