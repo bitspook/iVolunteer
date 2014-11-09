f8046157cd279cbfe021435749ff9bf97b528cd8
Jignesh Kakadiya <jignesh.kakadiya@inmobi.com>
2014-11-09 06:56:08 +0530

    add d3 charts

--- client/templates/js/admin_dashboard.js
+++ client/templates/js/admin_dashboard.js
@@ -1,20 +1,45 @@
 Template.AdminDashboard.helpers({
-	votes: function() {
-		return {
-			call: 23,
-			web: 98,
-			sms: 124
-		};
-	}
+    votes: function() {
+        return {
+            call: 23,
+            web: 98,
+            sms: 124
+        };
+    }
 });
 
 Template.AdminDashboard.events({
-	'click .remove': function(event, template) {
-		var id = $(event.currentTarget).data('id');
-		var nominee = Nominees.findOne(id);
-		var response = confirm('Are you sure you want to delete "' + nominee.first_name + '" ? This is an irreversible operation.');
+    'click .remove': function(event, template) {
+        var id = $(event.currentTarget).data('id');
+        var nominee = Nominees.findOne(id);
+        var response = confirm('Are you sure you want to delete "' + nominee.first_name + '" ? This is an irreversible operation.');
         if (!response) return false;
 
-		nominee.remove();
-	}
+        nominee.remove();
+    },
+
+    'click .nominee': function(event, template) {
+        event.preventDefault();
+
+        var id = $(event.currentTarget).data('id');
+        var nominee = Nominees.findOne(id);
+        var votes =  {
+            calls: 25,
+            sms: 25,
+            web: 50
+        }
+
+        var salesData=[
+            {label:"calls", color:"#3366CC"},
+            {label:"sms", color:"#FF9900"},
+            {label:"web", color:"#DC3912"}
+        ];
+
+        Donut3D.draw("d3-chart", randomData(), 150, 150, 130, 100, 30, 0.4);
+        function randomData(){
+            return salesData.map(function(d){
+                return { label:d.label, value: votes[d.label], color:d.color };
+            });
+        }
+    }
 })
